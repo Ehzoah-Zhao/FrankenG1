4ce435f83b532e9ccf3c3c92077ea0817ae20f98
Ehzoah <zhaohaozhe86@gmail.com>
2026-05-29 17:11:17 +0800

    fix: change length to tensor in user_input tx batch

--- generate_g1.py
+++ generate_g1.py
@@ -104,8 +104,8 @@ def generate(c: DictConfig):
             "keyid": ["user_input_0"],
         }
         batch["tx"]["mask"] = torch.ones(1, dtype=torch.bool).to(c.device)
-        batch["tx"]["length"] = 1
-        batch["tx_uncond_batch"] = {"x": torch.zeros(1, 512, device=c.device), "mask": torch.ones(1, dtype=torch.bool, device=c.device), "length": 1}
+        batch["tx"]["length"] = torch.tensor([1], device=c.device)
+        batch["tx_uncond_batch"] = {"x": torch.zeros(1, 512, device=c.device), "mask": torch.ones(1, dtype=torch.bool, device=c.device), "length": torch.tensor([1], device=c.device)}
         if "x" in tx_emb_dict:
             batch["tx"]["x"] = tx_emb_dict["x"].unsqueeze(0).to(c.device)
         infos = {
